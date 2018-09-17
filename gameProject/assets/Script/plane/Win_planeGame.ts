9313b7f1ea49c685cc3e77f46ceebeab2eb373ce
stanYQ <724760554@qq.com>
2018-09-17 19:57:08 +0800

    游戏背景滚动

---
+++ gameProject/assets/Script/plane/Win_planeGame.ts
@@ -0,0 +1,41 @@
+const { ccclass, property, menu } = cc._decorator;
+@ccclass
+export default class Win_planeGame extends  cc.Component {
+    @property({
+        type: cc.Node,
+    })
+    bg1: cc.Node = null;
+
+    @property({
+        type: cc.Node,
+    })
+    bg2: cc.Node = null;
+
+    @property({
+        tooltip: "移动速度"
+    })
+    bgSpeed: number = 0.6;
+
+    onLoad(){
+        this.bgList[0] = this.bg1;
+        this.bgList[1] = this.bg2;
+    }
+    update(dt){
+        this.bgMove(this.bgList, this.bgSpeed);
+    }
+
+    private bgMove(bgList, bgSpeed){
+        for(let index = 0; index < bgList.length; index ++){
+            bgList[index].y -= bgSpeed; 
+        }
+
+        if(bgList[0].y <= 20 - bgList[0].height){
+            bgList[0].y = 1136;
+        }
+
+        if (bgList[1].y <= 1156 - 2 * bgList[1].height) {
+            bgList[1].y = 1136;
+        }
+    }
+    private bgList: cc.Node[] = [];
+}
---
+++ gameProject/assets/Script/plane/Win_planeMain.ts
@@ -0,0 +1,39 @@
+const { ccclass, property, menu } = cc._decorator;
+@ccclass
+export default class Win_planeMain extends  cc.Component {
+    @property({
+        type: cc.Button,
+        tooltip: "开始游戏按钮"
+    })
+    startBtn: cc.Button = null;
+
+    @property({
+        type: cc.Button,
+        tooltip: "结束游戏按钮"
+    })
+    closeBtn: cc.Button = null;
+
+    @property({
+        type: cc.Button,
+        tooltip: "游戏帮助按钮"
+    })
+    helpBtn: cc.Button = null;
+
+    onLoad(){
+        this.startBtn.node.on("click", this.onStartBtnClicked, this);
+        this.closeBtn.node.on("click",this.onCloseBtnClicked, this);
+    }
+
+    private onStartBtnClicked(){
+        cc.director.loadScene('planeGame');
+    }
+
+    private onCloseBtnClicked(){
+        cc.director.loadScene('main');
+
+    }
+
+    private onHelpBtnClicked(){
+
+    }
+}
